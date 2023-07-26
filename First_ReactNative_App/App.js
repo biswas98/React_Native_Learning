55e51b040ae2685491b6d30da3e33c96cd424714
biswas98 <biswasbiplab98@gmail.com>
2023-07-26 21:19:16 +0530

    adding first react native project

---
+++ First_ReactNative_App/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,  Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This is my application</Text>
+
+      <Button
+        title="Learn More"
+      />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+});
