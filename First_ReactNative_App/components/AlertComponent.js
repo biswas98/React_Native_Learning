bad70308fbd2ac146ca94e6e5a2561640d53c4fe
biswas98 <biswasbiplab98@gmail.com>
2023-08-02 19:36:20 +0530

    enhancements in AlertComponent.js

--- First_ReactNative_App/components/AlertComponent.js
+++ First_ReactNative_App/components/AlertComponent.js
@@ -1,14 +1,18 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Alert} from 'react-native';
 import React from 'react';
 
 export default AlertComponent = () => {
     
-
+    const customAlert= () => Alert.alert(
+        'WoW',
+        'After all you have clicked on the button',
+        [{text: 'Want to close'}]
+    );
     
     return (
         <View>
-            <TouchableOpacity>
-                <Text>Click</Text>
+            <TouchableOpacity style={mySheet.btn} onPress={customAlert}>
+                <Text style={{textAlign:'center'}}>Click</Text>
             </TouchableOpacity>
         </View>
     );
@@ -16,7 +20,9 @@ export default AlertComponent = () => {
 
 const mySheet = StyleSheet.create({
     btn: {
+        width: 100,
         borderWidth: 1,
         padding: 10,
+        borderRadius: 20,
     }
 });
